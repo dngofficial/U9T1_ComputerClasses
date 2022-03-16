6d44da9582053b2c43bf20a3223cbfaddc7649a4
BT_1E10_17 <sample@bths.edu>
2022-03-16 13:31:49 -0400

    Initial commit

---
+++ src/Computer.java
@@ -0,0 +1,27 @@
+public class Computer
+{
+    private int screenSize;
+    private int memory;
+
+    public  Computer(int screenSize, int memory)
+    {
+        this.memory = memory;
+        this.screenSize = screenSize;
+    }
+
+    public int getScreenSize()
+    {
+        return screenSize;
+    }
+
+    public int getMemory()
+    {
+        return memory;
+    }
+
+    public void installOperatingSystem()
+    {
+        System.out.println("installing OS...");
+    }
+
+}
